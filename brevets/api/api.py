c1a166d3dff9d4b7557cdd7edcbadbb68dc96563
Haoran Zhang <hzhang9@testium.cs.uoregon.edu>
2021-05-30 20:30:16 -0700

    Save current work

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -1,14 +1,115 @@
-from flask import Flask,request
+from flask import Flask,request,redirect, url_for, flash, abort
 from flask_restful import Resource, Api
 import pymongo
 from pymongo import MongoClient
 import os
+from flask_login import (LoginManager, current_user, login_required, login_user, logout_user, UserMixin, confirm_login, fresh_login_required)
+from flask_wtf import FlaskForm as Form
+from wtforms import BooleanField, StringField, validators
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer \
+                          as Serializer, BadSignature, \
+                          SignatureExpired)
+import time
 
 app = Flask(__name__)
 api = Api(app)
 client= MongoClient('mongodb://'+os.environ['MONGODB_HOSTNAME'],27017)
 db=client.tododb
+app.config['SECRET_KEY']='Default secret'
 
+def generate_token(expiration=600):
+    s=Serializer(app,config['SECRET_KEY'],expires_in=expiration)
+    return None
+
+def verify_token(token):
+    s= Serializer()
+    try:
+        data= s.loads(token)
+    except SignatureExpired:
+        return None
+    except BadSignature:
+        return None
+    return "Success"
+
+def hash_password(password):
+    return pwd_context.encrypt(password)
+
+def verify_password(password, hashVal):
+    return pwd_context.verify(password, hashVal)
+
+class RegisterForm(Form):
+    username= StringField('Username',[
+        validators.Length(min=2,max=25,message=u"Little too short for a username."),
+        validators.InputRequired(u"Forget something?")])
+    password= StringField('Password',[
+        validators.Length(min=2,max=25,message=u"Little too short for a username."),
+        validators.InputRequired(u"Forget something?")])
+
+class LoginForm(Form):
+    username= StringField('Username',[
+        validators.Length(min=2,max=25,message=u"Little too short for a username."),
+        validators.InputRequired(u"Forget something?")])
+    password= StringField('Password',[
+        validators.Length(min=2,max=25,message=u"Little too short for a username."),
+        validators.InputRequired(u"Forget something?")])
+    remember= BooleanField('Remember me')
+
+class User(UserMixin):
+    def __init__(self, uid):
+        self.id = uid
+
+    def get_id(self):
+        return self.id
+
+login_manager= LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return None
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = registerForm()
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    return flask.render_template('login.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    return flask.render_template('login.html',form=form)
+
+@app.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out.")
+    return redirect(url_for("index"))
+
+@app.route('/token')
+def token():
+    return None
+
+@app.errorhandler(400)
+def error_400(e):
+    return render_template('400.html'),400
+
+@app.errorhandler(401)
+def error_401(e):
+    return render_template('401.html'),401
+
+'''-----------------------------------------------------------------------'''
 class listAJ(Resource):
     def get(self):
         top=request.args.get("top")#get top variable(get fail will became None)
